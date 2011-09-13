f4b1dbdd41d06fbf843ebb52d20ec8ec1c0e8d70
hub-cap <mbasnight@gmail.com>
2011-09-13 16:25:54 -0500

    Initial import of my terrible c++ code

---
+++ reddwarf-guest/include/guest.h
@@ -0,0 +1,42 @@
+#ifndef __NOVAGUEST
+#define __NOVAGUEST
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+#include <syslog.h>
+#include "mysql_connection.h"
+
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
+
+
+class Guest {
+    sql::Driver *driver;
+    sql::Connection *con;
+    sql::PreparedStatement *stmt;
+    sql::ResultSet *res;
+    
+    public:
+        Guest();
+        ~Guest();
+        std::string create_user(const std::string & username, const std::string & password);
+        std::string list_users();
+        std::string delete_user(const std::string & username);
+        std::string create_database(const std::string & database_name, const std::string & character_set, const std::string & collate);
+        std::string list_databases();
+        std::string delete_database(const std::string & database_name);
+        std::string enable_root();
+        std::string disable_root();
+        std::string is_root_enabled();
+};
+
+class MySQLUser {
+    std::string name;
+    std::string password;
+    std::string databases;
+};
+
+#endif //__NOVAGUEST
---
+++ reddwarf-guest/src/guest.cc
@@ -0,0 +1,74 @@
+#include "guest.h"
+using namespace std;
+
+Guest::Guest() {
+    driver = get_driver_instance();
+    con = driver->connect("tcp://127.0.0.1:3306", "root", "");
+    // Connect to the MySQL test database
+    con->setSchema("information_schema");
+}
+
+Guest::~Guest() {
+    delete res;
+    delete stmt;
+    delete con;
+}
+
+string Guest::create_user(const string & username, const string & password) {
+    stmt = con->prepareStatement("CREATE USER `?`@:host IDENTIFIED BY '?';");
+    stmt->setString(1, username.c_str());
+    stmt->setString(2, password.c_str());
+    stmt->executeUpdate();
+    stmt->close();
+    delete stmt;
+    
+    return "Guest::create_user method";
+}
+string Guest::list_users() {
+    stmt = con->prepareStatement("select User from mysql.user where host != 'localhost';");
+    res = stmt->executeQuery();
+    while (res->next()) {
+        syslog(LOG_INFO, "\t... MySQL replies: %s", res->getString("User").c_str());
+    }
+    res->close();
+    stmt->close();
+    delete res;
+    delete stmt;
+    
+    return "Guest::list_users method";    
+}
+string Guest::delete_user(const string & username) {
+    stmt = con->prepareStatement("DROP USER `?`");
+    stmt->setString(1, username);
+    stmt->executeUpdate();
+    stmt->close();
+    delete stmt;
+
+    return "Guest::delete_user method";
+}
+string Guest::create_database(const string & database_name, const string & character_set, const string & collate) {
+    stmt = con->prepareStatement("CREATE DATABASE IF NOT EXISTS `?` CHARACTER SET = ? COLLATE = ?;");
+    stmt->setString(1, database_name);
+    stmt->setString(2, character_set);
+    stmt->setString(3, collate);
+    stmt->executeUpdate();
+    stmt->close();
+    delete stmt;
+        
+    return "Guest::create_database method";
+}
+string Guest::list_databases() {
+    return "Guest::list_databases method";   
+}
+string Guest::delete_database(const string & database_name) {
+    return "Guest::delete_database method";
+}
+string Guest::enable_root() {
+    return "Guest::enable_root method";
+}
+string Guest::disable_root() {
+    return "Guest::disable_root method";
+}
+string Guest::is_root_enabled() {
+    return "Guest::is_root_enabled method";
+}

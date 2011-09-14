853715ad9534362deb8f22ac00d8755aad89493f
hub-cap <mbasnight@gmail.com>
2011-09-14 16:35:52 -0500

    Fixing the code up to not leak in the guest when sql exceptions happen, DOH!

--- src/guest.cc
+++ src/guest.cc
@@ -5,55 +5,79 @@ Guest::Guest() {
     driver = get_driver_instance();
     con = driver->connect("tcp://127.0.0.1:3306", "root", "");
     // Connect to the MySQL test database
-    con->setSchema("information_schema");
+    con->setSchema("mysql");
 }
 
 Guest::~Guest() {
-    delete res;
-    delete stmt;
     delete con;
 }
 
-string Guest::create_user(const string & username, const string & password) {
-    stmt = con->prepareStatement("CREATE USER `?`@:host IDENTIFIED BY '?';");
-    stmt->setString(1, username.c_str());
-    stmt->setString(2, password.c_str());
-    stmt->executeUpdate();
-    stmt->close();
-    delete stmt;
+string Guest::create_user(const string & username,const string & password, const string & host) {
+    sql::PreparedStatement *stmt;
+    try {
+        stmt = con->prepareStatement("insert into mysql.user (Host, User, Password) values (?, ?, password(?))");
+        stmt->setString(1, host.c_str());
+        stmt->setString(2, username.c_str());
+        stmt->setString(3, password.c_str());
+        stmt->execute();
+        stmt->close();
+        delete stmt;
+    } catch (sql::SQLException &e) {
+        delete stmt;
+        throw e;
+    }
     
     return "Guest::create_user method";
 }
 string Guest::list_users() {
-    stmt = con->prepareStatement("select User from mysql.user where host != 'localhost';");
-    res = stmt->executeQuery();
-    while (res->next()) {
-        syslog(LOG_INFO, "username: %s", res->getString("User").c_str());
+    sql::PreparedStatement *stmt;
+    sql::ResultSet *res;
+    try {
+        stmt = con->prepareStatement("select User from mysql.user where host != 'localhost';");
+        res = stmt->executeQuery();
+        while (res->next()) {
+            syslog(LOG_INFO, "username: %s", res->getString("User").c_str());
+        }
+        res->close();
+        stmt->close();
+        delete res;
+        delete stmt;
+    } catch (sql::SQLException &e) {
+        delete res;
+        delete stmt;
+        throw e;
     }
-    res->close();
-    stmt->close();
-    delete res;
-    delete stmt;
-    
     return "Guest::list_users method";    
 }
 string Guest::delete_user(const string & username) {
-    stmt = con->prepareStatement("DROP USER `?`");
-    stmt->setString(1, username);
-    stmt->executeUpdate();
-    stmt->close();
-    delete stmt;
+    sql::PreparedStatement *stmt;
+    try {
+        stmt = con->prepareStatement("DROP USER `?`");
+        stmt->setString(1, username);
+        stmt->executeUpdate();
+        stmt->close();
+        delete stmt;
+    } catch (sql::SQLException &e) {
+        delete stmt;
+        throw e;
+    }
 
     return "Guest::delete_user method";
 }
 string Guest::create_database(const string & database_name, const string & character_set, const string & collate) {
-    stmt = con->prepareStatement("CREATE DATABASE IF NOT EXISTS `?` CHARACTER SET = ? COLLATE = ?;");
-    stmt->setString(1, database_name);
-    stmt->setString(2, character_set);
-    stmt->setString(3, collate);
-    stmt->executeUpdate();
-    stmt->close();
-    delete stmt;
+    sql::PreparedStatement *stmt;
+    try {
+        stmt = con->prepareStatement("CREATE DATABASE IF NOT EXISTS `?` CHARACTER SET = ? COLLATE = ?;");
+        stmt->setString(1, database_name);
+        stmt->setString(2, character_set);
+        stmt->setString(3, collate);
+        stmt->executeUpdate();
+        stmt->close();
+        delete stmt;
+    } catch (sql::SQLException &e) {
+        delete stmt;
+        throw e;
+    }
         
     return "Guest::create_database method";
 }

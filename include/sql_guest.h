6034a3b3b8fbeb21db9dfc9f2d8855ab2d1e2b4b
Tim Simpson <tim.simpson@rackspace.com>
2011-09-16 14:02:18 -0500

    Changed most of the Guest class into MySqlGuest. Created a second class to handle sending and retrieving JSON messages to it. I had to use some macros but I think I cut down on duplication of data as much as I could have.

--- include/sql_guest.h
+++ include/sql_guest.h
@@ -1,5 +1,5 @@
-#ifndef __NOVAGUEST
-#define __NOVAGUEST
+#ifndef __NOVAGUEST_SQL_GUEST_H
+#define __NOVAGUEST_SQL_GUEST_H
 #include <cstdlib>
 #include <cstdio>
 #include <json/json.h>
@@ -50,30 +50,11 @@ typedef boost::shared_ptr<MySQLUser> MySQLUserPtr;
 typedef std::vector<MySQLUserPtr> MySQLUserList;
 typedef boost::shared_ptr<MySQLUserList> MySQLUserListPtr;
 
-class Guest;
-
-class MessageHandler {
-
-public:
-    MessageHandler();
-
-    void handle_message(json_object * json);
-
-private:
-    std::auto_ptr<Guest> guest;
-};
-
-class Guest {
-
-    typedef std::auto_ptr<sql::PreparedStatement> PreparedStatementPtr;
-
-    sql::Driver *driver;
-    sql::Connection *con;
-
+class MySqlGuest {
 
     public:
-        Guest();
-        ~Guest();
+        MySqlGuest();
+        ~MySqlGuest();
 
         std::string create_user(const std::string & username, const std::string & password, const std::string & host);
         // You must delete the users that come from this list using something like `vector.clear();`
@@ -85,7 +66,35 @@ class Guest {
         std::string enable_root();
         std::string disable_root();
         bool is_root_enabled();
+
+    private:
+        typedef std::auto_ptr<sql::PreparedStatement> PreparedStatementPtr;
+
+        sql::Driver *driver;
+        sql::Connection *con;
+
         PreparedStatementPtr prepare_statement(const char * text);
 };
 
-#endif //__NOVAGUEST
+typedef boost::shared_ptr<MySqlGuest> MySqlGuestPtr;
+
+class MySqlMessageHandler : public MessageHandler {
+
+    public:
+        MySqlMessageHandler(MySqlGuestPtr guest);
+
+        virtual json_object * handle_message(json_object * json);
+
+
+        typedef json_object * (* MethodPtr)(const MySqlGuestPtr &,
+                                            json_object *);
+        typedef std::map<std::string, MethodPtr> MethodMap;
+
+    private:
+
+        MySqlGuestPtr guest;
+        MethodMap methods;
+};
+
+
+#endif //__NOVAGUEST_SQL_GUEST_H

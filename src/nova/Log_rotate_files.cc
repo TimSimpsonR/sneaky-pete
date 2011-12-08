79892aec6c3edf0bfe07a9bf381006def922eb9d
Tim Simpson <tim.simpson@rackspace.com>
2011-12-07 19:16:16 -0600

    First commit of Operation Woodchuck (code name OW).

---
+++ src/nova/Log_rotate_files.cc
@@ -0,0 +1,69 @@
+#include "nova/Log.h"
+
+#include <boost/format.hpp>
+#include "nova/utils/io.h"
+#include <string>
+
+using boost::format;
+namespace io = nova::utils::io;
+using std::string;
+
+namespace nova {
+
+namespace {
+
+    class LogFileRotater {
+    public:
+
+        LogFileOptions options;
+
+        LogFileRotater(LogFileOptions options) : options(options) {
+        }
+
+        inline string file_path(int index) {
+            if (index > 0) {
+                return str(format("%s.%d") % options.path % index);
+            } else {
+                return options.path;
+            }
+        }
+
+        /** Returns the index ending the last file given the prefix file path and the
+         *  last possible index number if one was found. */
+        int oldest_log_file_index() {
+            for (int i = options.max_old_files; i > 0; i --) {
+                string possible_file = file_path(i);
+                if (io::is_file(possible_file.c_str())) {
+                    return i;
+                }
+            }
+            return 0;
+        }
+
+        void rotate() {
+            for (int i = oldest_log_file_index(); i >= 0; i --) {
+                string old_file = file_path(i);
+                if (io::is_file(old_file.c_str())) {
+                    if (i == options.max_old_files) {
+                        ::remove(old_file.c_str());
+                    } else {
+                        string new_file = file_path(i + 1);
+                        ::rename(old_file.c_str(), new_file.c_str());
+                    }
+                } else {
+                    // Just skip it...
+                }
+            }
+        }
+
+    };
+
+} // end anonymous namespace
+
+
+void Log::_rotate_files(LogFileOptions options) {
+    LogFileRotater rotater(options);
+    rotater.rotate();
+}
+
+} // end nova namespace
--- tests/log_tests.cc
+++ tests/log_tests.cc
@@ -1,44 +1,244 @@
 #define BOOST_TEST_MODULE ConfigFile_Tests
 #include <boost/test/unit_test.hpp>
+
+#include <boost/foreach.hpp>
+#include <boost/format.hpp>
+#include <fstream>
+#include <iostream>
 #include "nova/Log.h"
+#include "nova/utils/regex.h"
+#include <string>
+#include <boost/thread.hpp>
+#include <vector>
 
+// Confirm the macros works everywhere by not using nova::Log.
+using boost::format;
+using nova::LogException;
+using nova::LogFileOptions;
+using nova::LogOptions;
+using boost::optional;
+using nova::utils::Regex;
+using nova::utils::RegexMatchesPtr;
+using std::string;
+using std::vector;
 
-using nova::Log;
+#define CHECK_POINT() BOOST_CHECK_EQUAL(2,2);
 
-
-BOOST_AUTO_TEST_CASE(test_log_info2)
-{
-    Log log;
-    log.info2("this %s a test: %i", "is", 1);
-    log.info2("1234567890"
-    "1234567890"
-    "1234567890"
-    "1234567890"
-    "1234567890");
-    log.info2("this %s a test"
-    "A VERY LONG TEST"
-    "A VERY LONG TEST"
-    "A VERY LONG TEST"
-    "A VERY LONG TEST"
-    "A VERY LONG TEST"
-    "A VERY LONG TEST"
-    "A VERY LONG TEST"
-    "A VERY LONG TEST"
-    "A VERY LONG TEST"
-    "A VERY LONG TEST"
-    "A VERY LONG TEST"
-    "A VERY LONG TEST"
-    "A VERY LONG TEST"
-    "A VERY LONG TEST"
-    "A VERY LONG TEST"
-    "A VERY LONG TEST"
-    "A VERY LONG TEST"
-    ": %i", "is", 1);
-
-    std::string biggest;
-    for (int i = 0; i < 2048; i ++) {
-        biggest.append("A");
+#define CHECK_LOG_EXCEPTION(statement, ex_code) try { \
+        statement ; \
+        BOOST_FAIL("Should have thrown LogException."); \
+    } catch(const LogException & le) { \
+        const char * actual = LogException::code_to_string(LogException::ex_code); \
+        const char * expected = LogException::code_to_string(le.code); \
+        BOOST_REQUIRE_EQUAL(actual, expected); \
     }
 
-    log.info2("this is %s big test", biggest.c_str());
+BOOST_AUTO_TEST_CASE(shutdown_should_be_idempotent)
+{
+    nova::Log::shutdown();
+    nova::Log::shutdown();
 }
+
+BOOST_AUTO_TEST_CASE(should_throw_if_uninitialized)
+{
+    nova::Log::shutdown();
+
+    CHECK_LOG_EXCEPTION({
+        NOVA_LOG_DEBUG2("Arrggghhh!");
+    }, NOT_INITIALIZED);
+}
+
+
+static int test_count = 0;
+
+struct LogTestsFixture {
+
+    string log_file;
+
+    LogTestsFixture() : log_file(str(format("log_tests_%d") % test_count)) {
+        remove(log_file.c_str());
+        LogFileOptions file_options(log_file, 3);
+        LogOptions options(optional<LogFileOptions>(file_options), true, false);
+        nova::Log::initialize(options);
+        test_count ++;
+    }
+
+    ~LogTestsFixture() {
+        nova::Log::shutdown();
+    }
+
+    void read_file(vector<string> & lines) {
+        std::ifstream file(log_file.c_str());
+        while(!file.eof()) {
+            string line;
+            std::getline(file, line);
+            lines.push_back(line);
+        }
+        file.close();
+    }
+
+};
+
+BOOST_FIXTURE_TEST_SUITE(LogTestsSuite,
+                         LogTestsFixture);
+
+BOOST_AUTO_TEST_CASE(writing_some_lines) {
+    NOVA_LOG_DEBUG("Hello from the tests. How're you doing?");
+    NOVA_LOG_ERROR("Hi");
+    NOVA_LOG_INFO("Bye");
+
+    /* Read the log file and confirm it works. */
+    vector<string> lines;
+    read_file(lines);
+
+    {
+        Regex regex("[0-9]{4}-[0-9]{2}-[0-9]{2}\\s{1}[0-9]{2}:[0-9]{2}:[0-9]{2}"
+                    "\\s{1}DEBUG Hello from the tests\\. How\'re you doing\\? "
+                    "for tests/log_tests.cc:[0-9]+");
+        RegexMatchesPtr matches = regex.match(lines[0].c_str());
+        BOOST_CHECK_EQUAL(!!matches, true);
+    }
+
+    {
+        Regex regex("[0-9]{4}-[0-9]{2}-[0-9]");
+        RegexMatchesPtr matches = regex.match(lines[1].c_str());
+        BOOST_CHECK_EQUAL(!!matches, true);
+    }
+
+    {
+        Regex regex("[0-9]{4}-[0-9]{2}-[0-9]{2}\\s{1}[0-9]{2}:[0-9]{2}:[0-9]{2}"
+                "\\s{1}INFO  Bye "
+                "for tests/log_tests.cc:[0-9]+");
+        RegexMatchesPtr matches = regex.match(lines[2].c_str());
+        BOOST_CHECK_EQUAL(!!matches, true);
+    }
+}
+
+
+struct Worker {
+    int id;
+    char stage;
+    bool quit;
+
+    Worker(int id) : id(id), stage('a'), quit(false) {
+    }
+
+    void next_state() {
+        stage ++;
+    }
+
+    void work() {
+        while(!quit) {
+            std::cerr << "WORK id=" << id << " stage=" << stage << std::endl;
+            NOVA_LOG_DEBUG2("id=%d stage=%c", id, stage);
+            NOVA_LOG_ERROR2("id=%d stage=%c", id, stage);
+            NOVA_LOG_INFO2("id=%d stage=%c", id, stage);
+            boost::posix_time::seconds time(0);
+            boost::this_thread::sleep(time);
+        }
+    }
+};
+
+
+BOOST_AUTO_TEST_CASE(writing_lines_in_a_thread) {
+    std::cerr << "Welcome to Test #2." << std::endl;
+    NOVA_LOG_INFO("Welcome to Test #2.");
+    const int worker_count = 10;
+    vector<boost::thread *> threads;
+    vector<Worker> workers;
+    for (int i = 0; i < worker_count; i ++) {
+        workers.push_back(Worker(i));
+    }
+    for (int i = 0; i < worker_count; i ++) {
+        CHECK_POINT();
+        threads.push_back(new boost::thread(&Worker::work, &workers[i]));
+    }
+
+    CHECK_POINT();
+
+    boost::posix_time::seconds time(0);
+    boost::this_thread::sleep(time);
+    for (char s = 'a'; s <= 'e'; s = s + (char) 1) {
+        // We give all the workers one whole second to print out their letters.
+        CHECK_POINT();
+        boost::posix_time::seconds time(1);
+        boost::this_thread::sleep(time);
+        CHECK_POINT();
+        std::cout << "Time to enter a world of pain." << std::endl;
+        BOOST_FOREACH(Worker & worker, workers) {
+            CHECK_POINT();
+            std::cout << "next state for : " << worker.id << std::endl;
+            worker.next_state();
+        }
+    }
+
+    BOOST_CHECK_EQUAL(2,2);
+
+    BOOST_FOREACH(Worker & worker, workers) {
+        worker.quit = true;
+    }
+
+    BOOST_CHECK_EQUAL(2,2);
+
+    BOOST_FOREACH(boost::thread * thread, threads) {
+        thread->interrupt();
+        thread->join();
+        delete thread;
+    }
+
+    BOOST_CHECK_EQUAL(2,2);
+    boost::this_thread::sleep(boost::posix_time::seconds(10));
+}
+
+BOOST_AUTO_TEST_CASE(writing_lines_to_threads_while_switching_files) {
+    std::cerr << "Welcome to Test #3." << std::endl;
+    NOVA_LOG_INFO("Welcome to Test #3.");
+    const int worker_count = 50;
+    vector<boost::thread *> threads;
+    vector<Worker> workers;
+    for (int i = 20; i < worker_count + 20; i ++) {
+        workers.push_back(Worker(i));
+    }
+    for (int i = 20; i < worker_count + 20; i ++) {
+        CHECK_POINT();
+        threads.push_back(new boost::thread(&Worker::work, &workers[i]));
+    }
+
+    CHECK_POINT();
+
+    boost::posix_time::seconds time(0);
+    boost::this_thread::sleep(time);
+    for (char s = 'a'; s <= 'e'; s = s + (char) 1) {
+        // We give all the workers one whole second to print out their letters.
+        CHECK_POINT();
+        boost::posix_time::seconds time(1);
+        boost::this_thread::sleep(time);
+        CHECK_POINT();
+        std::cout << "Time for waffles." << std::endl;
+        BOOST_FOREACH(Worker & worker, workers) {
+            CHECK_POINT();
+            std::cout << "next state for : " << worker.id << std::endl;
+            worker.next_state();
+            nova::Log::rotate_files();
+        }
+    }
+
+    BOOST_CHECK_EQUAL(2,2);
+
+    BOOST_FOREACH(Worker & worker, workers) {
+        worker.quit = true;
+    }
+
+    BOOST_CHECK_EQUAL(2,2);
+
+    BOOST_FOREACH(boost::thread * thread, threads) {
+        thread->interrupt();
+        thread->join();
+        delete thread;
+    }
+
+    BOOST_CHECK_EQUAL(2,2);
+    boost::this_thread::sleep(boost::posix_time::seconds(10));
+}
+
+BOOST_AUTO_TEST_SUITE_END();

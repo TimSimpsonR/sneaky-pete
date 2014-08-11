856db5134b789a5e3f44588528d0b8e6c4482874
Tim Simpson <tim.simpson@rackspace.com>
2014-08-11 10:22:39 -0500

    Removing the redis backup demo... for now...?!

--- tests/redis_backup_demo.cc
+++ tests/redis_backup_demo.cc
@@ -1,40 +1,40 @@
-#include <stdio.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <fstream>
-#include <iostream>
-#include "nova/utils/Curl.h"
-#include <boost/format.hpp>
-#include "nova/Log.h"
-#include <boost/assign/list_of.hpp>
-#include "nova/guest/redis/client.h"
-#include "nova/redis/RedisBackup.h"
-#include "nova/redis/RedisBackupJob.h"
+// #include <stdio.h>
+// #include <fcntl.h>
+// #include <sys/stat.h>
+// #include <unistd.h>
+// #include <fstream>
+// #include <iostream>
+// #include "nova/utils/Curl.h"
+// #include <boost/format.hpp>
+// #include "nova/Log.h"
+// #include <boost/assign/list_of.hpp>
+// #include "nova/guest/redis/client.h"
+// #include "nova/redis/RedisBackup.h"
+// #include "nova/redis/RedisBackupJob.h"
 
-using namespace std;
-using namespace boost;
-using namespace boost::assign;
-using nova::Log;
-using nova::LogApiScope;
-using nova::LogOptions;
+// using namespace std;
+// using namespace boost;
+// using namespace boost::assign;
+// using nova::Log;
+// using nova::LogApiScope;
+// using nova::LogOptions;
 
-using nova::utils::Curl;
-using nova::utils::CurlScope;
+// using nova::utils::Curl;
+// using nova::utils::CurlScope;
 
-using namespace nova::redis;
+// using namespace nova::redis;
 
-using namespace nova::utils::swift;
-using namespace nova::redis;
+// using namespace nova::utils::swift;
+// using namespace nova::redis;
 
-int main(int argc, char **argv)
-{
-    LogApiScope log(LogOptions::simple());
-    CurlScope scope;
+// int main(int argc, char **argv)
+// {
+//     LogApiScope log(LogOptions::simple());
+//     CurlScope scope;
 
-    RedisBackupJob<Client> job;
+//     RedisBackupJob<Client> job;
 
-    job();
+//     job();
 
-    return 0;
-}
+//     return 0;
+// }

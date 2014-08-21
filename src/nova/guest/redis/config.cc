ad346e5fe29b01f4ca2fc8bfa4353b41c648cb25
Conrad Weidenkeller <conrad@weidenkeller.com>
2014-08-20 20:55:09 -0500

    Added code so strings with quotes get removed so hiredis will not escape them

--- src/nova/guest/redis/config.cc
+++ src/nova/guest/redis/config.cc
@@ -18,111 +18,105 @@ using nova::Log;
 using boost::optional;
 using std::string;
 
+namespace {
+
+std::string remove_quotes(const std::string & value) {
+    if(value.length() > 0 &&
+       value.at(0) == '"' &&
+       value.at(value.length() - 1) == '"') {
+        return value.substr(1, value.length() - 2);
+    }
+    return value;
+}
+}//end anon namespace
+
+
 
 namespace nova { namespace redis {
 
 
-std::string Config::_get_string_value(std::string key)
-{
-    if (_options.count(key) > 0)
-    {
+std::string Config::_get_string_value(std::string key) {
+    if (_options.count(key) > 0) {
         return _options[key];
     }
     return "";
 }
 
-int Config::_get_int_value(std::string key)
-{
-    if (_options.count(key) > 0)
-    {
+int Config::_get_int_value(std::string key) {
+    if (_options.count(key) > 0) {
         return boost::lexical_cast<int>(_options[key]);
     }
     return INT_NULL;
 }
 
 Config::Config(const optional<string> & config)
-:   _redis_config(config.get_value_or("/etc/redis/redis.conf"))
-{
+:   _redis_config(config.get_value_or("/etc/redis/redis.conf")) {
     std::string line;
     std::string value;
     std::ifstream rconfig(_redis_config.c_str());
-    if (!rconfig.is_open())
-    {
+    if (!rconfig.is_open()) {
         return;
     }
-    while(std::getline(rconfig, line))
-    {
-        if (line.length() <= 1)
-        {
+    while(std::getline(rconfig, line)) {
+        if (line.length() <= 1) {
             continue;
         }
         std::istringstream iss(line);
         iss >> value;
-        if(value == SLAVE_OF)
-        {
+        value = remove_quotes(value);
+        if(value == SLAVE_OF) {
             iss >> value;
-            _slave_of[MASTER_IP] = value;
+            _slave_of[MASTER_IP] = remove_quotes(value);
             iss >> value;
-            _slave_of[MASTER_PORT] = value;
+            _slave_of[MASTER_PORT] = remove_quotes(value);
         }
-        else
-        {
+        else {
             std::string option_value;
             iss >> option_value;
-            _options[value] = option_value;
+            _options[value] = remove_quotes(option_value);
         }
     }
     rconfig.close();
 
 }
 
-int Config::get_port()
-{
+int Config::get_port() {
     return _get_int_value(PORT);
 }
 
-std::string Config::get_pidfile()
-{
+std::string Config::get_pidfile() {
     return _get_string_value(PIDFILE);
 }
 
-std::string Config::get_db_filename()
-{
+std::string Config::get_db_filename() {
     return _get_string_value(DB_FILENAME);
 }
 
-std::string Config::get_db_dir()
-{
+std::string Config::get_db_dir() {
     return _get_string_value(DB_DIR);
 }
 
-std::map<std::string, std::string> Config::get_slave_of()
-{
+std::map<std::string, std::string> Config::get_slave_of() {
     return _slave_of;
 }
 
-std::string Config::get_master_auth()
-{
+std::string Config::get_master_auth() {
     return _get_string_value(MASTER_AUTH);
 }
 
-std::string Config::get_require_pass()
-{
+std::string Config::get_require_pass() {
     return _get_string_value(REQUIRE_PASS);
 }
 
-std::string Config::get_max_memory()
-{
+std::string Config::get_max_memory() {
     return _get_string_value(MAX_MEMORY);
 }
 
-std::string Config::get_append_filename()
-{
+std::string Config::get_append_filename() {
     return _get_string_value(APPEND_FILENAME);
 }
 
-std::string Config::get_log_file()
-{
+std::string Config::get_log_file() {
     return _get_string_value(LOG_FILE);
 }
 
